16caa2478c9e0c001d7318b02271a9757b9af346
wei.wang <wei.wang@cloudpense.com>
2019-06-11 20:15:23 +0800

    多人聊天测试

---
+++ src/main/java/com/zzus/netty/chatrom/MyChatServer.java
@@ -0,0 +1,30 @@
+package com.zzus.netty.chatrom;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @author wangwei
+ */
+public class MyChatServer {
+
+    public void run() throws Exception{
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new MyChatServerInializer());
+            ChannelFuture channelFuture = serverBootstrap.bind(7778).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
